09651bb7887f35dbf13844f4e2bff0928f77ec2e
Anton <noten.b@hotmail.com>
2021-12-22 14:17:21 +0100

    📦 Moved files

---
+++ AdventOfCode/AocSolution.cs
@@ -0,0 +1,30 @@
+﻿namespace AdventOfCode;
+
+internal abstract class AocSolution<T> {
+
+    public abstract string Name { get; }
+
+    public void RunPart1(string input) {
+        Part1(ProcessInput(input));
+    }
+
+    public void RunPart2(string input) {
+        Part2(ProcessInput(input));
+    }
+
+    protected abstract void Part1(T input);
+
+    public abstract void Part2(T input);
+
+    public virtual T ProcessInput(string input) {
+        if (input is T typedInput) {
+            return typedInput;
+        }
+
+        if (typeof(T) == typeof(string[])) {
+            return (T)(object)input.Split('\n');
+        }
+
+        throw new Exception($"Type not automatically implemented for type {typeof(T)}. Please override ProcessInput to make it the correct type!");
+    }
+}
