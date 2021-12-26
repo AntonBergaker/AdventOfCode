24ecd9f7fbd70485087b56cf6312fcfbdca173a1
Anton <noten.b@hotmail.com>
2021-12-26 14:29:39 +0100

    🐌 Snailfish math
    Can you tell I struggled on this one?

---
+++ AdventOfCode/Y2021/Day18/Snailfish.cs
@@ -0,0 +1,232 @@
+﻿namespace AdventOfCode.Y2021.Day18;
+
+public class Snailfish : AocSolution<string[]> {
+    public override string Name => "Snailfish";
+
+    protected override string Part1Implementation(string[] input) {
+        return $"Magnitude of sum: {GetSumForInput(input)}";
+    }
+
+    public long GetSumForInput(string[] input) {
+        SnailNumber number = ParseString(input[0]);
+
+        for (int i = 1; i < input.Length; i++) {
+            number = new Pair(number, ParseString(input[i]));
+            Reduce(number);
+        }
+        return number.GetMagnitude();
+    }
+
+    protected override string Part2Implementation(string[] input) {
+        // Test every combination!
+        long largestMagnitude = 0;
+        
+        foreach (string leftSide in input) {
+            foreach (string rightSide in input) {
+                var left = ParseString(leftSide);
+                var right = ParseString(rightSide);
+                var sum = new Pair(left, right);
+                Reduce(sum);
+                long magnitude = sum.GetMagnitude();
+                if (magnitude > largestMagnitude) {
+                    largestMagnitude = magnitude;
+                }
+            }
+        }
+
+        return $"Largest magnitude of summation of two: {largestMagnitude}";
+    }
+
+    public static void Reduce(SnailNumber number) {
+        bool Explode(SnailNumber number, int depth) {
+            if (number is Pair pair) {
+                if (depth >= 4) {
+                    NumberElement? left = GetLeft(pair);
+                    if (left != null) {
+                        left.Value += ((NumberElement)pair.Left).Value;
+                    }
+                    
+                    NumberElement? right = GetRight(pair);
+                    if (right != null) {
+                        right.Value += ((NumberElement)pair.Right).Value;
+                    }
+
+                    // Replace number with 0
+                    pair.Parent?.Replace(pair, new NumberElement(0));
+
+                    return true;
+                }
+                else {
+                    if (Explode(pair.Left, depth + 1)) return true;
+                    if (Explode(pair.Right, depth + 1)) return true;
+                }
+            }
+
+            return false;
+        }
+        bool Split(SnailNumber number) {
+
+            if (number is NumberElement element) {
+                if (element.Value >= 10) {
+                    element.Parent?.Replace(element, new Pair(
+                        new NumberElement(element.Value/2),
+                        new NumberElement(element.Value - element.Value/2)
+                    ));
+
+                    return true;
+                }
+            }
+            if (number is Pair pair) {
+                if (Split(pair.Left)) return true;
+                if (Split(pair.Right)) return true;
+            }
+
+
+            return false;
+        }
+
+        // Keep reducing until it returns false
+        while (true) {
+            if (Explode(number, 0)) {
+                continue;
+            }
+            if (Split(number)) {
+                continue;
+            }
+            break;
+        }
+    }
+
+    public static NumberElement? GetLeft(SnailNumber number) {
+        SnailNumber lastNumber = number;
+        Pair? parent = lastNumber.Parent;
+
+        while (parent != null) {
+            // If we're already leftmost, go out a step furher
+            if (parent.Left == lastNumber) {
+                lastNumber = parent;
+                parent = parent.Parent;
+                continue;
+            }
+
+            lastNumber = parent.Left;
+
+            // If we're not leftmost, go down right side until we find our first number
+            while (lastNumber is Pair childPair) {
+                lastNumber = childPair.Right;
+            }
+            return lastNumber as NumberElement;
+        }
+
+        return null;
+    }
+
+    public static NumberElement? GetRight(SnailNumber number) {
+        SnailNumber lastNumber = number;
+        Pair? parent = lastNumber.Parent;
+
+        while (parent != null) {
+            // If we're already rightmost, go out a step furher
+            if (parent.Right == lastNumber) {
+                lastNumber = parent;
+                parent = parent.Parent;
+                continue;
+            }
+
+            lastNumber = parent.Right;
+
+            // If we're not rightmost, go down left side until we find our first number
+            while (lastNumber is Pair childPair) {
+                lastNumber = childPair.Left;
+            }
+            return lastNumber as NumberElement;
+        }
+
+        return null;
+    }
+
+    public static SnailNumber ParseString(string input) {
+        SnailNumber ParseStringImplementation(string input, ref int index) {
+            if (input[index] == '[') {
+                index++; // skip [
+                SnailNumber left = ParseStringImplementation(input, ref index);
+                index++; // skip comma
+                SnailNumber right = ParseStringImplementation(input, ref index);
+                index++; // skip ]
+                Pair pair = new Pair(left, right);
+                return pair;
+            }
+            else {
+                string digits = "";
+                while (index < input.Length-1) {
+                    if (char.IsDigit(input[index]) == false) {
+                        break;
+                    }
+                    digits += input[index++];
+                }
+                int number = int.Parse(digits);
+                return new NumberElement(number);
+            }
+        }
+
+        int index = 0;
+        return ParseStringImplementation(input, ref index);
+    }
+
+    public abstract class SnailNumber {
+        public Pair? Parent;
+
+        public abstract long GetMagnitude();
+    }
+
+    public class Pair : SnailNumber {
+
+        public SnailNumber Left;
+        public SnailNumber Right;
+
+        public Pair(SnailNumber left, SnailNumber right) {
+            Left = left;
+            Left.Parent = this;
+            Right = right;
+            Right.Parent = this;
+        }
+
+        public void Replace(SnailNumber old, SnailNumber @new) {
+            @new.Parent = this;
+            if (Left == old) {
+                Left = @new;
+            }
+            else if (Right == old) {
+                Right = @new;
+            }
+            else {
+                throw new Exception("Old was neither child.");
+            }
+        }
+
+        public override string ToString() {
+            return $"[{Left},{Right}]";
+        }
+
+        public override long GetMagnitude() {
+            return (Left.GetMagnitude() * 3 + Right.GetMagnitude() * 2);
+        }
+    }
+
+    public class NumberElement : SnailNumber {
+        public long Value;
+
+        public NumberElement(long value) {
+            Value = value;
+        }
+
+        public override string ToString() {
+            return Value.ToString();
+        }
+
+        public override long GetMagnitude() {
+            return Value;
+        }
+    }
+
+}
---
+++ UnitTests/Y2021/Day18/SnailfishTests.cs
@@ -0,0 +1,128 @@
+﻿using AdventOfCode;
+using AdventOfCode.Y2021.Day18;
+using NUnit.Framework;
+using System;
+
+namespace UnitTest.Y2021.Day18;
+
+ static class SnailfishHelpers {
+    public static Snailfish.SnailNumber LeftChild(this Snailfish.SnailNumber number) {
+        if (number is not Snailfish.Pair pair) {
+            throw new Exception("Not a pair");
+        }
+        return (pair.Left);
+    }
+
+    public static Snailfish.SnailNumber RightChild(this Snailfish.SnailNumber number) {
+        if (number is not Snailfish.Pair pair) {
+            throw new Exception("Not a pair");
+        }
+        return (pair.Right);
+    }
+}
+
+internal class SnailfishTests {
+
+
+    [Test]
+    public void TestSnailfishGetLeft() {
+        {
+            var pair = new Snailfish.Pair(
+                new Snailfish.NumberElement(1),
+                new Snailfish.NumberElement(2)
+            );
+
+            Assert.AreEqual(pair.Left, Snailfish.GetLeft(pair.Right));
+        }
+
+        {
+            var pair = new Snailfish.Pair(
+                new Snailfish.NumberElement(2),
+                new Snailfish.Pair(
+                    new Snailfish.NumberElement(1),
+                    new Snailfish.NumberElement(3)
+                )
+            );
+
+            Assert.AreEqual(pair.Left, Snailfish.GetLeft(pair.Right.LeftChild()));
+
+            Assert.AreEqual(pair.Right.LeftChild(), Snailfish.GetLeft(pair.Right.RightChild()));
+        }
+
+        {
+            var pair = new Snailfish.Pair(
+                new Snailfish.Pair(
+                    new Snailfish.NumberElement(2),
+                    new Snailfish.NumberElement(4)
+                ),
+                new Snailfish.Pair(
+                    new Snailfish.NumberElement(1),
+                    new Snailfish.NumberElement(3)
+                )
+            );
+
+            Assert.AreEqual(pair.Left.RightChild(), Snailfish.GetLeft(pair.Right.LeftChild()));
+        }
+    }
+
+    [Test]
+    public void TestReduceExplode() {
+       void AssertReducedToExpected(string expected, string input) {
+            var number = Snailfish.ParseString(input);
+            Snailfish.Reduce(number);
+            Assert.AreEqual(expected, number.ToString());
+        }
+        
+        AssertReducedToExpected("[[[[0,9],2],3],4]", "[[[[[9,8],1],2],3],4]");
+        AssertReducedToExpected("[7,[6,[5,[7,0]]]]", "[7,[6,[5,[4,[3,2]]]]]");
+        AssertReducedToExpected("[[6,[5,[7,0]]],3]", "[[6,[5,[4,[3,2]]]],1]");
+        AssertReducedToExpected("[[3,[2,[8,0]]],[9,[5,[7,0]]]]", "[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]");
+    }
+
+    [Test]
+    public void TestReduceSplit() {
+        var number = Snailfish.ParseString("[4,11]");
+        Snailfish.Reduce(number);
+        Assert.AreEqual("[4,[5,6]]", number.ToString());
+    }
+
+
+    [Test]
+    public void TestMagnitude() {
+        var pair = new Snailfish.Pair(
+            new Snailfish.Pair(
+                new Snailfish.NumberElement(1),
+                new Snailfish.NumberElement(2)
+            ),
+            new Snailfish.Pair(
+                new Snailfish.Pair(
+                    new Snailfish.NumberElement(3),
+                    new Snailfish.NumberElement(4)
+                ),
+                new Snailfish.NumberElement(5)
+            )
+        );
+
+        Assert.AreEqual(143L, pair.GetMagnitude());
+    }
+
+    [Test]
+    public void TestSampleInput() {
+        var snailfish = new Snailfish();
+
+        var sample =
+            "[[[0,[5,8]],[[1,7],[9,6]]],[[4,[1,2]],[[1,4],2]]]\n" +
+            "[[[5,[2,8]],4],[5,[[9,9],0]]]\n" +
+            "[6,[[[6,2],[5,6]],[[7,6],[4,7]]]]\n" +
+            "[[[6,[0,7]],[0,9]],[4,[9,[9,0]]]]\n" +
+            "[[[7,[6,4]],[3,[1,3]]],[[[5,5],1],9]]\n" +
+            "[[6,[[7,3],[3,2]]],[[[3,8],[5,7]],4]]\n" +
+            "[[[[5,4],[7,7]],8],[[8,3],8]]\n" +
+            "[[9,3],[[9,9],[6,[4,9]]]]\n" +
+            "[[2,[[7,7],7]],[[5,8],[[9,3],[0,2]]]]\n" +
+            "[[[[5,2],5],[8,[3,7]]],[[5,[7,5]],[4,4]]]\n";
+
+
+        Assert.AreEqual(4140L, snailfish.GetSumForInput(sample.SplitLines()));
+    }
+}
